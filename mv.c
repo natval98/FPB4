35825229bc5fb459a5e27a10845b28aff32c405a
Ayas Faikar Nafis <ayasfn@gmail.com>
2017-12-06 00:51:47 +0700

    updated : saat error tidak buat file/hapus file

--- mv.c
+++ mv.c
@@ -7,26 +7,25 @@ char buf[512];
 
 int main (int argc, char *argv[])
 {
-    int fd0, fd1, n, flag=0;
+    int fd0, fd1, n;
     if (argc <= 2){
         printf(1,"Need 2 arguments! mv [From] [To]\n");
-        flag = 1;
         exit();
     }
     if ((fd0 = open (argv[1], O_RDONLY)) < 0){
         printf(1, "mv: cannot open %s\n", argv[1]);
-        flag =1;
+        exit();
     }
     if ((fd1 = open (argv[2], O_CREATE|O_RDWR)) < 0){
         printf(1, "mv: cannot open %s\n", argv[2]);
-        flag = 1;
+        exit();
     }
     
     while ( ( n = read (fd0, buf, sizeof(buf) ) ) > 0 ){
         write (fd1, buf, n );
     }
     
-    if (flag == 0) unlink(argv[1]);
+    unlink(argv[1]);
     close(fd0);
     close(fd1);
     exit();
