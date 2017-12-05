6e2a1c233854a3d1c60c20e0202e9806b136579d
Natasha Valentina <32843032+natval98@users.noreply.github.com>
2017-12-05 10:31:27 +0700

    touch.c sdh bisa jalan

--- touch.c
+++ touch.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+#include "fs.h"
 
-int main(int argc, char *argv[]) {
-
-	int i;
-	
-	argc -= optind;
-	argv += optind;
-
-	for (i = 0; *argv; ++argv)
-	{	
-		int fd = open(*argv, O_WRONLY | O_CREAT, DEFFILEMODE);
-
-		if (fd != -1){
-			close (fd);
-			i = 1;
-			continue;
-		}
+int main( int argc, char *argv[])
+{
+	int f;
+	if (argc < 2){
+		printf(2, "Need 2 arguments, Touch [file]");
+		exit();
 	}
-    return 0;
+	if (( f = open(argv[1], O_CREATE | O_RDWR)) < 0){
+		printf(1, "touch: %s failed to create\n", argv[1]);
+	}
+	close(f);
+	exit();
 }
