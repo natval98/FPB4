1e4559c1cb414478ec7b81058178ce4e36b5246a
ArdourHope <32842753+ArdourHope@users.noreply.github.com>
2017-12-07 22:15:47 +0700

    Update rm.c

--- rm.c
+++ rm.c
@@ -1,19 +1,28 @@
 #include <syscall.h>
 #include <stdio.h>
+#include <string.h>
+
+
+#include <xv6/stdio.h>
+#include <xv6/dirent.h>
+#include <xv6/stat.h>
+#include <xv6/fcntl.h>
+#include <xv6/fs.h>
+
 
 int
 main(int argc, char *argv[])
 {
   int i;
 
-  if(argc < 2){
-    fprintf(stderr, "Usage: rm files...\n");
+  if(argc == 1 ){
+    printf("Usage: rm files...\n");
     sysexit();
   }
-
+  if
   for(i = 1; i < argc; i++){
     if(unlink(argv[i]) < 0){
-      fprintf(stderr, "rm: %s failed to delete\n", argv[i]);
+      printf("rm: %s failed to delete\n", argv[i]);
       break;
     }
   }
