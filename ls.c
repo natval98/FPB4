4a5ed512ac44024f0ab22c7b0a2a61386adc21d5
itsmerafi <32842792+itsmerafi@users.noreply.github.com>
2017-12-07 20:08:49 +0700

    Update ls.c

--- ls.c
+++ ls.c
@@ -1,24 +1,41 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-#include "fs.h"
+#include <syscall.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <xv6/stdio.h>
+#include <xv6/dirent.h>
+#include <xv6/stat.h>
+#include <xv6/fcntl.h>
+
+int pathstat(char *n, struct stat *st)
+{
+  int fd;
+  int r;
+
+  fd = open(n, O_RDONLY);
+  if(fd < 0)
+    return -1;
+  r = fstat(fd, st);
+  close(fd);
+  return r;
+}
 
 char*
 fmtname(char *path)
 {
-  static char buf[DIRSIZ+1];
+  static char buf[NAME_MAX+1];
   char *p;
-
+  
   // Find first character after last slash.
   for(p=path+strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
-
+  
   // Return blank-padded name.
-  if(strlen(p) >= DIRSIZ)
+  if(strlen(p) >= NAME_MAX)
     return p;
   memmove(buf, p, strlen(p));
-  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  memset(buf+strlen(p), ' ', NAME_MAX-strlen(p));
   return buf;
 }
 
@@ -29,41 +46,41 @@ ls(char *path)
   int fd;
   struct dirent de;
   struct stat st;
-
+  
   if((fd = open(path, 0)) < 0){
-    printf(2, "ls: cannot open %s\n", path);
- 	return;
+    fprintf(stderr, "ls: cannot open %s\n", path);
+    return;
   }
-
+  
   if(fstat(fd, &st) < 0){
-    printf(2, "ls: cannot stat %s\n", path);
+    fprintf(stderr, "ls: cannot stat %s\n", path);
     close(fd);
     return;
   }
-
+  
   switch(st.type){
   case T_FILE:
-    printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+    printf("%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
     break;
-
+  
   case T_DIR:
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf(1, "ls: path too long\n");
+    if(strlen(path) + 1 + NAME_MAX + 1 > sizeof buf){
+      fprintf(stderr, "ls: path too long\n");
       break;
     }
     strcpy(buf, path);
     p = buf+strlen(buf);
     *p++ = '/';
     while(read(fd, &de, sizeof(de)) == sizeof(de)){
-      if(de.inum == 0)
+      if(de.d_ino == 0)
         continue;
-      memmove(p, de.name, DIRSIZ);
-      p[DIRSIZ] = 0;
-      if(stat(buf, &st) < 0){
-        printf(1, "ls: cannot stat %s\n", buf);
+      memmove(p, de.d_name, NAME_MAX);
+      p[NAME_MAX] = 0;
+      if(pathstat(buf, &st) < 0){
+        fprintf(stderr, "ls: cannot stat %s\n", buf);
         continue;
       }
-      printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
     }
     break;
   }
@@ -77,9 +94,9 @@ main(int argc, char *argv[])
 
   if(argc < 2){
     ls(".");
-    exit();
+    sysexit();
   }
   for(i=1; i<argc; i++)
     ls(argv[i]);
-  exit();
+  sysexit();
 }
