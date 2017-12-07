0f09b9ed3004de412d80ffd1842a5c026730ba99
ArdourHope <32842753+ArdourHope@users.noreply.github.com>
2017-12-07 22:55:00 +0700

    Update rm.c

--- rm.c
+++ rm.c
@@ -9,6 +9,36 @@
 #include <xv6/fcntl.h>
 #include <xv6/fs.h>
 
+int type_file (char * file)
+{
+  int salah=-1;
+  int file=1;
+  int dir=0;
+  struct stat status;
+  int fh; //file handler
+  fh = open (file,o_RDONLY);
+  if (fh <= -1) // bukan file & tidak bisa di open
+  {
+    return salah;
+  }
+  if(fstat(fd, &status) < 0) //tidak bisa dibuka dan status tidak bisa di lihat
+  {
+    return salah;
+  }
+  if (status.type == T_FILE)
+  {
+    return file;
+  }
+  else if (status.type == T_DIR)
+  {
+    return dir;
+  }
+  else 
+    return salah;
+ )
+
+
+
 
 int
 main(int argc, char *argv[])
@@ -19,7 +49,6 @@ main(int argc, char *argv[])
     printf("Usage: rm files...\n");
     sysexit();
   }
-  if
   for(i = 1; i < argc; i++){
     if(unlink(argv[i]) < 0){
       printf("rm: %s failed to delete\n", argv[i]);
